897020874077715d46d45983e6584676ef79b3fe
howard.reith@gmail.com <howard.reith@gmail.com>
2021-08-10 19:22:08 -0400

    Add some styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import React from 'react';
 import './App.css';
+import {
+  Box, makeStyles,
+} from '@material-ui/core';
 import MbtaBoardPage from './components/mbtaBoard/MbtaBoardPage';
+import backgroundImage from './resources/northSationBackground.jpeg';
+
+const useStyles = makeStyles({
+  appWrapper: {
+    backgroundImage: `url(${backgroundImage})`,
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: '100% 100%',
+    height: '100vh',
+    width: '100%',
+    margin: 0,
+  },
+});
 
 function App() {
+  const classes = useStyles();
   return (
-    <div className="App">
-      <MbtaBoardPage />
-    </div>
+    <Box className={classes.appWrapper}>
+      <Box>
+        <MbtaBoardPage />
+      </Box>
+    </Box>
   );
 }
 
--- src/components/mbtaBoard/MbtaBoardPage.js
+++ src/components/mbtaBoard/MbtaBoardPage.js
@@ -1,7 +1,36 @@
 import React, { useEffect, useState } from 'react';
+import {
+  TableContainer, Table, TableHead, TableRow, TableCell, TableBody, makeStyles, Box,
+} from '@material-ui/core';
+
+const useStyles = makeStyles({
+  boxContainer: {
+    backgroundColor: 'black',
+    marginLeft: '20px',
+    marginRight: '20px',
+  },
+  table: {
+    minWidth: 650,
+  },
+  tableHeading: {
+    color: 'gray',
+    paddingTop: '2px',
+    paddingBottom: '2px',
+    borderBottom: '1px solid darkgray',
+  },
+  tableCell: {
+    color: 'gold',
+    fontFamily: 'Lucida Console',
+    fontSize: '16px',
+    paddingTop: '2px',
+    paddingBottom: '2px',
+    borderBottom: '1px solid darkgray',
+  },
+});
 
 export default function MbtaBoardPage() {
   const [data, setData] = useState(null);
+  const classes = useStyles();
   // Carrier, time, destination, train#, track#, status
   // Also displays date in the upper left, time in the upper right
   useEffect(() => {
@@ -18,6 +47,9 @@ export default function MbtaBoardPage() {
           formattedData[key] = {};
           formattedData[key].departureTime = datum.attributes.departure_time;
           formattedData[key].destination = datum.relationships.route.data.id.replace('CR-', '');
+          formattedData[key].trackNumber = 'TBD';
+          formattedData[key].trainNumber = 'TBD';
+          formattedData[key].status = 'On time';
         });
       })
       .then(() => {
@@ -31,17 +63,18 @@ export default function MbtaBoardPage() {
                 if (datum.attributes.departure_time) {
                   formattedData[key].departureTime = datum.attributes.departure_time;
                 }
+                formattedData[key].name = key;
                 formattedData[key].status = datum.attributes.status;
-                formattedData[key].trainNumber = datum.relationships.vehicle.data
-                && datum.relationships.vehicle.data.id;
-                formattedData[key].tracknumber = stopData.length > 2 ? stopData[2] : null;
+                formattedData[key].trainNumber = (datum.relationships.vehicle.data
+                  && datum.relationships.vehicle.data.id) || 'TBD';
+                formattedData[key].trackNumber = stopData.length > 2 ? stopData[2] : 'TBD';
               }
             });
+            // TODO fix this linter silliness
+            // eslint-disable-next-line max-len
+            const sorted = Object.values(formattedData).sort((a, b) => (new Date(a.departureTime)) - (new Date(b.departureTime)));
+            setData(sorted);
           });
-        // TODO fix this linter silliness
-        // eslint-disable-next-line max-len
-        const sorted = Object.values(formattedData).sort((a, b) => (new Date(a.departureTime)) - (new Date(b.departureTime)));
-        setData(sorted);
       })
       .catch((err) => {
         // eslint-disable-next-line no-console
@@ -49,7 +82,43 @@ export default function MbtaBoardPage() {
       });
   }, []);
 
-  console.log('===> data', data);
-
-  return <div>I am here</div>;
+  return (
+    <Box className={classes.boxContainer}>
+      <Box align="left">
+        {/* <h2></h2> */}
+      </Box>
+      <TableContainer>
+        <Table className={classes.table}>
+          <TableHead>
+            <TableRow>
+              <TableCell className={classes.tableHeading} align="center">Carrier</TableCell>
+              <TableCell className={classes.tableHeading} align="center">Time</TableCell>
+              <TableCell className={classes.tableHeading} align="center">Destination</TableCell>
+              <TableCell className={classes.tableHeading} align="center">Train#</TableCell>
+              <TableCell className={classes.tableHeading} align="center">Track</TableCell>
+              <TableCell className={classes.tableHeading} align="center">Status</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {data && data.map((row) => {
+              let time = new Date(row.departureTime).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+              if (time[0] === '0') {
+                time = time.replace('0', '');
+              }
+              return (
+                <TableRow key={row.name}>
+                  <TableCell className={classes.tableCell} align="left">MBTA</TableCell>
+                  <TableCell className={classes.tableCell} align="left">{time}</TableCell>
+                  <TableCell className={classes.tableCell} align="left">{row.destination}</TableCell>
+                  <TableCell className={classes.tableCell} align="left">{row.trainNumber}</TableCell>
+                  <TableCell className={classes.tableCell} align="left">{row.trackNumber}</TableCell>
+                  <TableCell className={classes.tableCell} align="left">{row.status}</TableCell>
+                </TableRow>
+              );
+            })}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Box>
+  );
 }
