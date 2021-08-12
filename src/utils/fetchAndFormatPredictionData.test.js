0a3578135f5c2dbece99a1c691d37b5691c23817
howard.reith@gmail.com <howard.reith@gmail.com>
2021-08-12 17:12:23 -0400

    Add github pages. Fix the fetchAndFormat test.

--- src/utils/fetchAndFormatPredictionData.test.js
+++ src/utils/fetchAndFormatPredictionData.test.js
@@ -14,18 +14,41 @@ describe('fetchAndFormatPredictionData', () => {
   });
 
   it('should sort and format the data', async () => {
-    // TODO There is something amiss with this test, there should be more
-    const result = await fetchAndFormatPredictionData(mockSchedulesResponse);
-    const expected = [
-      {
-        departureTime: '2021-08-11T17:05:00-04:00',
-        name: 'CR-479172-117-MRRockport-BNT-0000-04-0',
-        destination: 'Newburyport',
-        status: 'Now boarding',
-        trainNumber: '1650',
-        trackNumber: '04',
-      },
-    ];
+    const scheduleData = {};
+    mockSchedulesResponse.data.forEach((datum) => {
+      const key = datum.id.replace('schedule-', '');
+      scheduleData[key] = {};
+      scheduleData[key].departureTime = datum.attributes.departure_time;
+      scheduleData[key].destination = datum.relationships.route.data.id.replace('CR-', '');
+    });
+    const result = await fetchAndFormatPredictionData(scheduleData);
+    const expected = [{
+      departureTime: '2021-08-11T17:05:00-04:00', name: 'CR-479172-117-MRRockport-BNT-0000-04-0', destination: 'Newburyport', status: 'Now boarding', trainNumber: '1650', trackNumber: '04',
+    }, {
+      departureTime: '2021-08-11T17:10:00-04:00', name: 'CR-479277-295-BNT-0000-0', destination: 'Haverhill', status: 'On time', trainNumber: 'TBD', trackNumber: 'TBD',
+    }, {
+      departureTime: '2021-08-11T17:25:00-04:00', name: 'CR-479309-329-BNT-0000-0', destination: 'Lowell', status: 'On time', trainNumber: 'TBD', trackNumber: 'TBD',
+    }, {
+      departureTime: '2021-08-11T17:30:00-04:00', name: 'CR-479347-425-BNT-0000-0', destination: 'Fitchburg', status: 'On time', trainNumber: 'TBD', trackNumber: 'TBD',
+    }, {
+      departureTime: '2021-08-11T17:35:00-04:00', name: 'CR-479228-159-MRRockport-BNT-0000-0', destination: 'Newburyport', status: 'On time', trainNumber: 'TBD', trackNumber: 'TBD',
+    }, {
+      departureTime: '2021-08-11T17:40:00-04:00', name: 'CR-479255-217-BNT-0000-0', destination: 'Haverhill', status: 'On time', trainNumber: 'TBD', trackNumber: 'TBD',
+    }, {
+      departureTime: '2021-08-11T17:55:00-04:00', name: 'CR-479279-297-BNT-0000-0', destination: 'Haverhill', status: 'On time', trainNumber: 'TBD', trackNumber: 'TBD',
+    }, {
+      departureTime: '2021-08-11T18:05:00-04:00', name: 'CR-479205-119-MRRockport-BNT-0000-0', destination: 'Newburyport', status: 'On time', trainNumber: 'TBD', trackNumber: 'TBD',
+    }, {
+      departureTime: '2021-08-11T18:10:00-04:00', name: 'CR-479311-331-BNT-0000-0', destination: 'Lowell', status: 'On time', trainNumber: 'TBD', trackNumber: 'TBD',
+    }, {
+      departureTime: '2021-08-11T18:30:00-04:00', name: 'CR-479348-427-BNT-0000-0', destination: 'Fitchburg', status: 'On time', trainNumber: 'TBD', trackNumber: 'TBD',
+    }, {
+      departureTime: '2021-08-11T18:35:00-04:00', name: 'CR-479230-161-MRRockport-BNT-0000-0', destination: 'Newburyport', status: 'On time', trainNumber: 'TBD', trackNumber: 'TBD',
+    }, {
+      departureTime: '2021-08-11T18:40:00-04:00', name: 'CR-479257-219-BNT-0000-0', destination: 'Haverhill', status: 'On time', trainNumber: 'TBD', trackNumber: 'TBD',
+    }, {
+      departureTime: '2021-08-11T18:55:00-04:00', name: 'CR-479313-333-BNT-0000-0', destination: 'Lowell', status: 'On time', trainNumber: 'TBD', trackNumber: 'TBD',
+    }];
     expect(result).toEqual(expected);
   });
 });
